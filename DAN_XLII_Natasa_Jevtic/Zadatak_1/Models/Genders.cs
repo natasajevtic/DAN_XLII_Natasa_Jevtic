6fb211d01d87e505769e0e7120d2be546c5deaef
natasajevtic <nataly.jevtic.95@gmail.com>
2020-07-01 10:56:46 +0200

    Class Genders created

---
+++ DAN_XLII_Natasa_Jevtic/Zadatak_1/Models/Genders.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Zadatak_1.Models
+{
+    class Genders
+    {
+        /// <summary>
+        /// This method creates a list of data from view of genders.
+        /// </summary>
+        /// <returns></returns>
+        public List<vwGender> GetAllGender()
+        {
+            try
+            {
+                using (Employee_DataEntities context = new Employee_DataEntities())
+                {
+                    List<vwGender> genders = new List<vwGender>();
+                    genders = (from x in context.vwGenders select x).ToList();
+                    return genders;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+    }
+}
